f9a3af50d24986d529868e4be949bc4d8fe267ba
Nana Adjei Manu <claeusdev@gmail.com>
2017-11-20 20:23:42 +0000

    Fix location bug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
   def index
   	@recent_places = Place.all.order(created_at: :desc)
-  	@near_by_places = Place.near([visitor_lat, visitor_long], 20)
   end
 
   def about
